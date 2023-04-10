a8c04f415f905fbec908de4d4ba4f62c72c455cf
chenxuehai@alltuu.com <cxh741@qq.com>
2023-04-10 16:55:01 +0800

    feat: eslint configuration

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,6 @@
 import { defineBuildConfig } from 'unbuild'
 // import fg from 'fast-glob'
 
-
 export default defineBuildConfig({
   entries: [
     // ...fg.sync('src/*.ts').map(i => i.slice(0, -3)),
@@ -11,6 +10,6 @@ export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true,
-    inlineDependencies: true,
-  },
+    inlineDependencies: true
+  }
 })
